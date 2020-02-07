f4427eae23a81d04e9735358abfaa73644c19cbc
Fredrik Lillejordet <fretholil@gmail.com>
2020-02-07 21:27:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res) {
   res.send("Dette kommer fra backend")
 })
 
+app.get("/folk", function(req, res) {
+  res.send("Dette er en liste over folk")
+})
+
 app.listen(PORT, function() {
   console.log(`Listening on Port ${PORT}`)
 })
