2197d1a682bb50e45a23d5b079e1999fefdef1aa
ane.sporastoyl <ane.sporastoyl@bekk.no>
2020-02-08 11:59:07 +0100

    Tok bort utropstegn

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 
 app.get("/", function(req, res) {
-  res.send("Dette kommer fra backend!")
+  res.send("Dette kommer fra backend")
 })
 
 app.get("/folk", function(req, res) {
